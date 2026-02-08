21fef2879ae270a61a9c1198e1e04bf0872e55ea
Murat Eker <emailadresin@example.com>
2026-02-08 20:31:58 +0300

    fix: autostart feature

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# 1. Masaüstü arayüzünün kendine gelmesi için 10 saniye bekle
+sleep 10
+
+# 3. Sanal ortamı aktif et
+source main_env/bin/activate
+
+# 2. Klasöre git
 cd /home/murat/Desktop/lavida
-source venv/bin/activate
-python main.py
+
+
+# 4. Uygulamayı başlat ve tüm çıktıları log dosyasına yaz
+# (Hata olursa bu dosyanın içine bakacağız)
+python main.py > startup_log.txt 2>&1
